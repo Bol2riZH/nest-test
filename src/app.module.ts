d95befbe313be0e80db4e611cff76820c9ee0294
Matthieu Reungoat <reungoat.matthieu@gmail.com>
2024-08-21 16:37:56 +0200

    Add: logger

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { EmployeesModule } from './employees/employees.module';
 import { UsersModule } from './users/users.module';
+import { MyLoggerModule } from './my-logger/my-logger.module';
 
 @Module({
   imports: [
@@ -16,6 +17,7 @@ import { UsersModule } from './users/users.module';
       { name: 'short', ttl: 60000, limit: 3 },
       { name: 'long', ttl: 60000, limit: 10 },
     ]),
+    MyLoggerModule,
   ],
   controllers: [AppController],
   providers: [AppService, { provide: APP_GUARD, useClass: ThrottlerGuard }],
